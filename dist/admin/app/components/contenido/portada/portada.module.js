69d7808d0f83a099c058d1d13bde097ee82e2a48
Oliver Castelblanco <ocastelblanco@gmail.com>
2016-07-27 00:17:01 +0000

    Carga medios en entrada

--- dist/admin/app/components/contenido/portada/portada.module.js
+++ dist/admin/app/components/contenido/portada/portada.module.js
@@ -1,9 +1,44 @@
-/* global angular */
+/* global angular firebase */
 var portada = angular.module('portada', []);
-portada.controller('articuloDestacado', ['cargaInterfaz', function(cargaInterfaz){
-    //console.log('Artidulo destacado cargado');
+portada.controller('articuloDestacado', ['cargaInterfaz', '$timeout', function(cargaInterfaz, $timeout){
     var salida = this;
-    cargaInterfaz.textos().then(function(resp){
-        salida.opciones = resp.contenido.portada.articuloDestacado.opciones;
+    var ruta = 'portada/articuloDestacado';
+    salida.igual = function(op1, op2) {
+        if (op1 == op2) {return true;}else{return false;}
+    };
+    firebase.database().ref(ruta).once('value').then(function(snapshot) {
+        salida.sel = snapshot.val();
+        cargaInterfaz.textos().then(function(resp){
+            salida.opciones = resp.contenido.portada.articuloDestacado.opciones;
+        });
     });
+    salida.cambio = function(index){
+        salida.cargando = true;
+        firebase.database().ref(ruta).set(index).then(function(){
+            $timeout(function(){salida.cargando = false;},500);
+        }).catch(function(val){
+            console.log('Error: ', val);
+        });
+    };
+}]);
+portada.controller('medioDestacado', ['cargaInterfaz', '$timeout', function(cargaInterfaz, $timeout){
+    var salida = this;
+    var ruta = 'portada/medioDestacado';
+    salida.igual = function(op1, op2) {
+        if (op1 == op2) {return true;}else{return false;}
+    };
+    firebase.database().ref(ruta).once('value').then(function(snapshot) {
+        salida.sel = snapshot.val();
+        cargaInterfaz.textos().then(function(resp){
+            salida.opciones = resp.contenido.portada.medioDestacado.opciones;
+        });
+    });
+    salida.cambio = function(index){
+        salida.cargando = true;
+        firebase.database().ref(ruta).set(index).then(function(){
+            $timeout(function(){salida.cargando = false;},500);
+        }).catch(function(val){
+            console.log('Error: ', val);
+        });
+    };
 }]);
